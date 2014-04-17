65ca6a7726a6424dcfbda3579dc92f593223fc54
Matthew Swartwout <mws85@case.edu>
2014-04-17 11:11:27 -0400

    ADC->DAC Lab Fully Functioning
    Lab4 is fully functioning. Removes an error that was causing the volume
    to be greatly reduced (bits of data were being placed in the DAC's don't
    care bits).

--- Lab4/DAC.v
+++ Lab4/DAC.v
@@ -43,10 +43,8 @@ always @(posedge dacSerialClock)
 		
 		else
 			begin
-						if (count < 3)
-							dacDataIn = 0;
 							
-						else if (count < 5)
+						if (count < 5)
 							dacDataIn = 0;
 						
 						else if (count < 6)
@@ -56,56 +54,45 @@ always @(posedge dacSerialClock)
 							dacDataIn = 0;
 							
 						else if (count < 12)
-							dacDataIn = 0;
-							
-						else if (count < 13)
-							dacDataIn = 0;
-							
-						else if (count < 14)
-							dacDataIn = 0;
-							
-						else if (count < 15)
-							dacDataIn = 0;
-							
-						else if (count < 16)
 							dacDataIn = adcDataOutPackage[11];
 							
-						else if (count < 17)
+						else if (count < 13)
 							dacDataIn = adcDataOutPackage[10];
 							
-						else if (count < 18)
+						else if (count < 14)
 							dacDataIn = adcDataOutPackage[9];
 							
-						else if (count < 19)
+						else if (count < 15)
 							dacDataIn = adcDataOutPackage[8];
 							
-						else if (count < 20)
+						else if (count < 16)
 							dacDataIn = adcDataOutPackage[7];
 							
-						else if (count < 21)
+						else if (count < 17)
 							dacDataIn = adcDataOutPackage[6];
 							
-						else if (count < 22)
+						else if (count < 18)
 							dacDataIn = adcDataOutPackage[5];
 							
-						else if (count < 23)
+						else if (count < 19)
 							dacDataIn = adcDataOutPackage[4];
 							
-						else if (count < 24)
+						else if (count < 20)
 							dacDataIn = adcDataOutPackage[3];
 							
-						else if (count < 25)
+						else if (count < 21)
 							dacDataIn = adcDataOutPackage[2];
 							
-						else if (count < 26)
+						else if (count < 22)
 							dacDataIn = adcDataOutPackage[1];
 							
-						else if (count < 27)
+						else if (count < 23)
 							dacDataIn = adcDataOutPackage[0];
 							
-						else if (count < 31)
+						else if (count < 32)
 							dacDataIn = 0;
 			end
+			
 		count = count + 1;
 		
 		end
