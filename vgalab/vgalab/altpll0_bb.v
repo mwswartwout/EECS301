755e7ab8aea94b43ec349ac20221d3826171f5bd
Ishaan Taylor <taylor.ishaan@gmail.com>
2014-04-27 16:16:01 -0400

    changed clock to 9 mhz, planned counter mapping for user input

--- vgalab/vgalab/altpll0_bb.v
+++ vgalab/vgalab/altpll0_bb.v
@@ -63,7 +63,7 @@ endmodule
 // Retrieval info: PRIVATE: DEVICE_SPEED_GRADE STRING "6"
 // Retrieval info: PRIVATE: DIV_FACTOR0 NUMERIC "1"
 // Retrieval info: PRIVATE: DUTY_CYCLE0 STRING "50.00000000"
-// Retrieval info: PRIVATE: EFF_OUTPUT_FREQ_VALUE0 STRING "25.174999"
+// Retrieval info: PRIVATE: EFF_OUTPUT_FREQ_VALUE0 STRING "9.000000"
 // Retrieval info: PRIVATE: EXPLICIT_SWITCHOVER_COUNTER STRING "0"
 // Retrieval info: PRIVATE: EXT_FEEDBACK_RADIO STRING "0"
 // Retrieval info: PRIVATE: GLOCKED_COUNTER_EDIT_CHANGED STRING "1"
@@ -88,7 +88,7 @@ endmodule
 // Retrieval info: PRIVATE: MIRROR_CLK0 STRING "0"
 // Retrieval info: PRIVATE: MULT_FACTOR0 NUMERIC "1"
 // Retrieval info: PRIVATE: NORMAL_MODE_RADIO STRING "1"
-// Retrieval info: PRIVATE: OUTPUT_FREQ0 STRING "25.17500000"
+// Retrieval info: PRIVATE: OUTPUT_FREQ0 STRING "9.00000000"
 // Retrieval info: PRIVATE: OUTPUT_FREQ_MODE0 STRING "1"
 // Retrieval info: PRIVATE: OUTPUT_FREQ_UNIT0 STRING "MHz"
 // Retrieval info: PRIVATE: PHASE_RECONFIG_FEATURE_ENABLED STRING "1"
@@ -127,9 +127,9 @@ endmodule
 // Retrieval info: PRIVATE: ZERO_DELAY_RADIO STRING "0"
 // Retrieval info: LIBRARY: altera_mf altera_mf.altera_mf_components.all
 // Retrieval info: CONSTANT: BANDWIDTH_TYPE STRING "AUTO"
-// Retrieval info: CONSTANT: CLK0_DIVIDE_BY NUMERIC "2000"
+// Retrieval info: CONSTANT: CLK0_DIVIDE_BY NUMERIC "50"
 // Retrieval info: CONSTANT: CLK0_DUTY_CYCLE NUMERIC "50"
-// Retrieval info: CONSTANT: CLK0_MULTIPLY_BY NUMERIC "1007"
+// Retrieval info: CONSTANT: CLK0_MULTIPLY_BY NUMERIC "9"
 // Retrieval info: CONSTANT: CLK0_PHASE_SHIFT STRING "0"
 // Retrieval info: CONSTANT: COMPENSATE_CLOCK STRING "CLK0"
 // Retrieval info: CONSTANT: INCLK0_INPUT_FREQUENCY NUMERIC "20000"
@@ -192,7 +192,7 @@ endmodule
 // Retrieval info: GEN_FILE: TYPE_NORMAL altpll0.ppf TRUE
 // Retrieval info: GEN_FILE: TYPE_NORMAL altpll0.inc FALSE
 // Retrieval info: GEN_FILE: TYPE_NORMAL altpll0.cmp FALSE
-// Retrieval info: GEN_FILE: TYPE_NORMAL altpll0.bsf TRUE FALSE
+// Retrieval info: GEN_FILE: TYPE_NORMAL altpll0.bsf TRUE
 // Retrieval info: GEN_FILE: TYPE_NORMAL altpll0_inst.v FALSE
 // Retrieval info: GEN_FILE: TYPE_NORMAL altpll0_bb.v TRUE
 // Retrieval info: LIB_FILE: altera_mf
---
+++ vgalab/vgalab/userInput.v
@@ -0,0 +1,18 @@
+module userInput();
+
+// will be a counter from 0 - 12
+/*
+	0: null
+	1:0
+	2:1
+	3:2
+	...
+	9:8
+	10:9
+	11:+
+	12:-
+	13:=
+*/
+
+
+endmodule
