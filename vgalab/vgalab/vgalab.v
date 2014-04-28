0001ea2239f0600f8bf067c171111f286a11191b
Matthew Swartwout <mws85@case.edu>
2014-04-28 16:38:24 -0400

    Added clk3Hz
    clk3Hz was left out of clocking instantiation. This commit also adds in
    .qpf files.

--- vgalab/vgalab/vgalab.v
+++ vgalab/vgalab/vgalab.v
@@ -6,8 +6,8 @@ wire hsync, vsync;
 wire [3:0] inCount;
 wire clk3Hz;
 
-cloking cloking(clk50mhz, vgaclk, hsync, vsync);                    		// controls the clocks that determine timing
-altpll0 altpll0(clk50mhz, vgaclk);                     						// generates 25.175 MHz clock
+cloking cloking(clk50mhz, vgaclk, hsync, vsync, hden, vden, clk3Hz);    // controls the clocks that determine timing
+altpll0 altpll0(clk50mhz, vgaclk);                     						// generates 9 MHz clock
 userInput userInput(button1, button0, inCount, clk3Hz);						// controls the user input
 
 endmodule
