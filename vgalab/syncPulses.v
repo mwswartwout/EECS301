e4278b9994d160f641679f76029a73868606486c
Matthew Swartwout <mws85@case.edu>
2014-05-05 16:29:47 -0400

    Major Update 5/5
    Heavy file clean-up. Replaces clocking.v with syncPulses, which is
    essentially the same. Pulses should be correctly aligned. Removes
    pixelOutput because it is not doing anything currently, and removes
    startUp because it is not necessary. Everything currently compiles
    properly.

---
+++ vgalab/syncPulses.v
@@ -0,0 +1,93 @@
+module syncPulses(clk9MHz, hSync, vSync, hData, vData, clk3Hz);
+
+input clk9MHz;          
+
+integer vgaCount, lineCount, vcount, hzcount;
+reg hData, vData;			// horizontal data enable, vertical data enable
+//reg start;					// to output 10 black frames at the start
+
+output reg hSync, vSync;    	// 640 clk9MHz to 1 hSync, 480 hSync to 1 vSync
+										// OR, timing diagram says 
+														// 41+484 clk9MHz to 1 hSync
+														// 10+276 hSync to 1 vSync
+output hData, vData;		// enable signals for data transfer
+output reg clk3Hz;		// character change clock 3 Hz
+
+				
+initial
+	begin	
+		// startCount = 0;
+		hSync = 0;
+		vSync = 0;
+		hData  = 0;
+		vData  = 0;
+		vgaCount = 0;
+		lineCount   = 0;
+		vcount   = 0;	// TODO: determine if vcount is needed
+		hzcount = 0;
+		clk3Hz  = 0;
+	end
+
+	
+always @(posedge clk9MHz)
+	begin
+		// vgaCount counts clock cycles and controls hSync pulses, vgaCount can also be used to determine the horizontal location of the pixel being set
+		vgaCount = vgaCount + 1;
+		if (vgaCount == 0)
+			hSync = 0;
+		if (vgaCount == 41)
+			hSync = 1;
+		if (vgaCount == 43)
+			hData = 1;
+		if (vgaCount == 523)		// TODO: figure out if this is 523 or 524, in timing diagram it looks like data must be held until rising edge of next cycle
+			hData = 0;
+		if (vgaCount == 525)					// if vga reaches its cycle
+			begin	
+				vgaCount = 0;						// reset vgaCount
+				hSync = 0;				// invert hSync
+				lineCount = lineCount + 1;				// increment lineCount
+			end
+		
+		// lineCount controls vSync
+		if (lineCount == 0)
+			vSync = 0;
+		if (lineCount == 10)
+			vSync = 1;
+		if (lineCount == 12)
+			vData = 1;
+		if (lineCount == 284)		// TODO: figure out if this is 284 or 285, in timing diagram it looks like data must be held until rising edge of next cycle
+			vData = 0;
+		if (lineCount == 286)
+			begin	
+				lineCount = 0;
+				vSync = 0;
+				vcount = vcount + 1;
+			end
+			
+		/* I can't see why this is necessary
+		if (vcount == 10) 		// hard reset everything but "start"
+			begin	
+				hSync = 0;
+				vSync = 0;
+				hData  = 0;
+				vData  = 0;
+				vgaCount = 0;
+				lineCount   = 0;
+				vcount   = 0;	
+				hzcount = 0;
+				clk3Hz  = 0;
+			end
+		*/
+		
+		// clock for user input
+		hzcount = hzcount + 1;
+		if (hzcount == 4000000)
+			begin	
+				clk3Hz = ~clk3Hz;
+				hzcount = 0;
+			end
+			
+	end
+	
+
+endmodule
--- vgalab/vgalab.v
+++ vgalab/vgalab.v
@@ -1,18 +1,15 @@
-module vgalab(clk50mhz, button1, button0);
+module vgalab(clk50MHz, button1, button0);
 
-input clk50mhz, button1, button0;															// inputs
-wire vgaclk;																						// 9 MHz clock that the LCD gathers data with
-wire hsync, vsync;																				// pulses that tell the LCD pixel pointer when to horizontally or vertically reset the pointer
+input clk50MHz, button1, button0;															// inputs
+wire clk9MHz;																						// 9 MHz clock that the LCD gathers data with
+wire hSync, vSync, hData, vData;																				// pulses that tell the LCD pixel pointer when to horizontally or vertically reset the pointer
 wire [3:0] inCount;																				// counter that determines which character to display
 wire clk3Hz;																						// clk for gathering user input (on rising edge)
 wire enable;																						// enable for all data (hi after 10 black frames)
 // output wire vcc, avdd, disp, lcdDisp;					/** NOT SURE WHAT TO DO HERE */
 
-cloking 		cloking(clk50mhz, vgaclk, hsync, vsync, hden, vden, clk3Hz);    		// controls: the clocks that determine overall timing
-altpll0 		altpll0(clk50mhz, vgaclk);                     								// generates: 9 MHz clock
-startUp 		startUp(vcc, avdd, clk50mhz, vgaclk, disp, lcdDisp, enable);			// powers up: the LCD display properly
+syncPulses 	syncPulses(clk9MHz, hSync, vSync, hData, vData, clk3Hz);    		// controls: the clocks that determine overall timing
+altpll0 			PLL(clk50MHz, clk9MHz);                     								// generates: 9 MHz clock
 userInput 	userInput(button1, button0, inCount, clk3Hz);								// controls: the user input
-pixelOutput pixelOutput(enable, inCount, vgaclk, hsync, vsync, hden, vden);		// controls: placing the pixels to the right place at the right time
-
 
 endmodule
