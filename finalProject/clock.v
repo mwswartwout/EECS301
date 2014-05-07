3529d7724e8480bdd9c51133c76113df3033eb25
Matthew Swartwout <mws85@case.edu>
2014-05-07 12:25:58 -0400

    ADC/DAC Sample Rate to 50kHz
    Sets the delay within the clocking block to set the ADC/DAC at a 50kHz
    sample rate.

--- finalProject/clock.v
+++ finalProject/clock.v
@@ -14,16 +14,16 @@ always @(posedge serialClock)
 	
 		//Control block for syncDAC pulse
 		countSync = countSync + 1;
-		if (countSync == 32)
+		if (countSync == 32) //DAC takes 32-bit input
 			syncDAC = 1;
 			
 		//Control block for syncADC pulse
-		if (countSync == 16)
+		if (countSync == 16) //ADC takes 16-bit input
 			syncADC = 1;
 			
-		else if (countSync == 64) //Delay makes it so that syncADC and syncDAC pulses are aligned
+		else if (countSync == 320) //Delay makes it so that syncADC and syncDAC pulses are aligned, and also so that they are operating at 50,000 samples per second.
 			begin
-				syncADC = 0;
+				syncADC = 0; //Reset all of the syncPulses and the counter
 				syncDAC = 0;
 				countSync = 0;
 			end
--- finalProject/finalProject.v
+++ finalProject/finalProject.v
@@ -7,8 +7,9 @@ wire [12:0] adcDataOutPackage;
 
 output wire adcSerialClock, dacSerialClock, lcdClock, adcDataIn, ldac, dacDataIn;
 
-PLL	PLL(fpgaClock, adcSerialClock, dacSerialClock, lcdClock);
-ADC	ADC(adcSerialClock, syncADC, adcDataOut, adcDataOutPackage, adcDataIn);
-DAC	DAC(/*adcDataOutPackage went here in wire lab, this should change*/, dacSerialClock, ldac, dacDataIn, syncDAC);
-clock	clock(dacSerialClock, syncDAC, syncADC);	
+PLL	PLL(fpgaClock, adcSerialClock, dacSerialClock, lcdClock); //PLL built using Altera MegaFunction; Input is 50MHz clock from FPGA, output is two 16MHz serial clocks (for DAC & ADC) and a 9MHz clock for the LCD.
+ADC	ADC(adcSerialClock, syncADC, adcDataOut, adcDataOutPackage, adcDataIn); //ADC block sends dataIn to the ADC and takes dataOut and packages it.
+DAC	DAC(/*adcDataOutPackage went here in wire lab, this should change*/, dacSerialClock, ldac, dacDataIn, syncDAC); //DAC block packages data for DAC and sends it
+clock	clock(dacSerialClock, syncDAC, syncADC); //Clock creates the sync pulses for the ADC and DAC so that they are receiving and outputting data at the same time
+highPass highPass()	
 endmodule
