7301f361d6d1c8c6693e242e883509ec934fc17e
Matthew Swartwout <mws85@case.edu>
2014-05-07 13:13:47 -0400

    Instantiate syncPulses
    Instantiates syncPulses. Removes hData & vData from syncPulses because
    they were unnecessary and not being used. Removes clk3hz and that logic
    because it is not necessary.

--- finalProject/finalProject.v
+++ finalProject/finalProject.v
@@ -1,4 +1,4 @@
-module finalProject(fpgaClock, adcDataOut, adcSerialClock, dacSerialClock, lcdClock, ldac, adcDataIn, dacDataIn);
+module finalProject(fpgaClock, adcDataOut, adcSerialClock, dacSerialClock, lcdClock, ldac, adcDataIn, dacDataIn, disp, redPixels, greenPixels, bluePixels);
 
 input wire fpgaClock, adcDataOut;
 
@@ -7,8 +7,13 @@ wire [11:0] adcDataOutPackage; //Data coming out of the ADC after it has been pa
 wire sinkValid, highPassOutValid, lowPassOutValid; //Valid signals from the filters (sinkValid = input, outValid = output)
 wire [11:0] highPassOutput, lowPassOutput; //12-bit output from the filters
 wire [1:0]	highPassError, lowPassError; //Output error from the filters, these are ignored completely
+wire start;	//Forces pixelOutput to display 10 black frames upon start-up of LCD screen
+wire hSync, vSync; //hSync and vSync pulses for the LCD screen
+wire [9:0] vgaCount; //10-bit counter that tracks horizontal location of the pixel being drawn
+wire [8:0] lineCount; //9-bit counter that tracks the vertical location of the pixel being drawn
 
-output wire adcSerialClock, dacSerialClock, lcdClock, adcDataIn, ldac, dacDataIn;
+output wire adcSerialClock, dacSerialClock, lcdClock, adcDataIn, ldac, dacDataIn, disp;
+output wire [7:0] redPixels, greenPixels, bluePixels; //8-bit values for setting RGB pixels on the LCD screen
 
 PLL	PLL(fpgaClock, adcSerialClock, dacSerialClock, lcdClock); //PLL built using Altera MegaFunction; Input is 50MHz clock from FPGA, output is two 16MHz serial clocks (for DAC & ADC) and a 9MHz clock for the LCD.
 ADC	ADC(adcSerialClock, syncADC, adcDataOut, adcDataOutPackage, adcDataIn); //ADC block sends dataIn to the ADC and takes dataOut and packages it.
@@ -17,5 +22,6 @@ clock	clock(dacSerialClock, syncDAC, syncADC, sinkValid); //Clock creates the sy
 highPass highPass(dacSerialClock, 1, adcDataOutPackage, sinkValid, 2'b00, highPassOutput, highPassOutValid, highPassError); //FIR high pass filter, coefficients obtained using Altera FIR Compiler I
 lowPass	lowPass(dacSerialClock, 1, adcDataOutPackage, sinkValid, 2'b00, lowPassOutput, lowPassOutValid, lowPassError);	//FIR low pass filter, coefficients obtained using online FIR coefficient calculator
 pixelOutput	pixelOutput(lcdClock, highPassOutput, lowPassOutput, redPixels, greenPixels, bluePixels, vgaCount, lineCount, start); //Determines what colors are being set for the pixel being drawn
+syncPulses	syncPulses(lcdClock, hSync, vSync, disp, vgaCount, lineCount, start); //Creates the hSync & vSync pulses for the LCD, and output vgaCount & lineCount which track which pixel is being drawn
 
 endmodule
--- finalProject/syncPulses.v
+++ finalProject/syncPulses.v
@@ -1,8 +1,8 @@
-module syncPulses(clk9MHz, hSync, vSync, hData, vData, clk3Hz, disp, vgaCount, lineCount, start);
+module syncPulses(clk9MHz, hSync, vSync, disp, vgaCount, lineCount, start);
 
 input clk9MHz;          
 
-integer vcount, hzcount;
+//integer vcount;
 output reg [9:0] vgaCount;
 output reg [8:0] lineCount;
 reg hData, vData;			// horizontal data enable, vertical data enable
@@ -12,20 +12,16 @@ output reg hSync, vSync;    	// 640 clk9MHz to 1 hSync, 480 hSync to 1 vSync
 										// OR, timing diagram says 
 														// 41+484 clk9MHz to 1 hSync
 														// 10+276 hSync to 1 vSync
-output hData, vData;		// enable signals for data transfer
-output reg clk3Hz, disp, start;		// character change clock 3 Hz
+														
+output reg disp, start;		// character change clock 3 Hz
 
 				
 initial
 	begin	
 		hSync = 0;
 		vSync = 1;
-		hData  = 0;
-		vData  = 0;
 		vgaCount	= 0;
 		lineCount = 0;
-		hzcount = 0;
-		clk3Hz  = 0;
 		disp = 0;
 		start = 0;
 		startCount = 0;
@@ -52,10 +48,6 @@ always @(posedge clk9MHz)
 					hSync = 0;
 				if (vgaCount == 41)
 					hSync = 1;
-				if (vgaCount == 43)
-					hData = 1;
-				if (vgaCount == 523)		// TODO: figure out if this is 523 or 524, in timing diagram it looks like data must be held until rising edge of next cycle
-					hData = 0;
 				if (vgaCount == 525)					// if vga reaches its cycle
 					begin	
 						vgaCount = 0;						// reset vgaCount
@@ -68,26 +60,12 @@ always @(posedge clk9MHz)
 					vSync = 0;
 				if (lineCount == 10)
 					vSync = 1;
-				if (lineCount == 12)
-					vData = 1;
-				if (lineCount == 284)		// TODO: figure out if this is 284 or 285, in timing diagram it looks like data must be held until rising edge of next cycle
-					vData = 0;
 				if (lineCount == 286)
 					begin	
 						lineCount = 0;
 						vSync = 0;
 					end
-			end
-		
-		// clock for user input
-		hzcount = hzcount + 1;
-		if (hzcount == 4000000)
-			begin	
-				clk3Hz = ~clk3Hz;
-				hzcount = 0;
-			end
-			
+			end	
 	end
-	
 
 endmodule
