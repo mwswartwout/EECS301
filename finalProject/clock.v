48c5ac41ac29f039bc27eeb2e8befc0e5b94da3a
Matthew Swartwout <mws85@case.edu>
2014-05-07 12:45:11 -0400

    sinkValid update
    Updates sinkValid so that it is high while no new data is being read,
    and low while new data is being input to the ADC

--- finalProject/clock.v
+++ finalProject/clock.v
@@ -1,10 +1,10 @@
-module clock(serialClock, syncDAC, syncADC);
+module clock(serialClock, syncDAC, syncADC, sinkValid);
 
 input serialClock; //16 MHz
 
 integer countSync;
 
-output reg syncDAC, syncADC;
+output reg syncDAC, syncADC, sinkValid;
 
 initial
 		countSync = 0;
@@ -19,12 +19,16 @@ always @(posedge serialClock)
 			
 		//Control block for syncADC pulse
 		if (countSync == 16) //ADC takes 16-bit input
-			syncADC = 1;
+			begin
+				syncADC = 1;
+				sinkValid = 1;
+			end
 			
 		else if (countSync == 320) //Delay makes it so that syncADC and syncDAC pulses are aligned, and also so that they are operating at 50,000 samples per second.
 			begin
 				syncADC = 0; //Reset all of the syncPulses and the counter
 				syncDAC = 0;
+				sinkValid = 0;
 				countSync = 0;
 			end
 	end
